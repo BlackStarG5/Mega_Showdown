cc1e129b296a463f659d6e23d1f16a09942833a4
yajatkaul <mc.crafter1973@gmail.com>
2026-03-30 19:48:10 +0530

    Hard reset command now resets gmax and tera as well without needing OP

--- common/src/main/java/com/github/yajatkaul/mega_showdown/gimmick/MaxGimmick.java
+++ common/src/main/java/com/github/yajatkaul/mega_showdown/gimmick/MaxGimmick.java
@@ -43,7 +43,11 @@ public record MaxGimmick(
         pokemon.getEntity().setGlowingTag(false);
 
         float startScale = pokemon.getScaleModifier();
-        ScalingData scalingData = new ScalingData(startScale, pokemon.getPersistentData().getFloat("orignal_size"), DEFAULT_SCALING_DURATION);
+        float targetScale = pokemon.getPersistentData().getFloat("orignal_size");
+        if (targetScale == 0) {
+            return;
+        }
+        ScalingData scalingData = new ScalingData(startScale, targetScale, DEFAULT_SCALING_DURATION);
 
         ACTIVE_SCALING_ANIMATIONS.put(pokemon, scalingData);
     }
